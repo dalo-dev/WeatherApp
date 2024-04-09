2366cf06d5bfe733459d6e3bbd8f6db08952ae45
DALO <dalodev@proton.me>
2024-04-09 08:36:07 -0500

    Added forecast API call

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,15 @@
 "use strict";
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const apiUrl = "https://api.openweathermap.org/data/2.5";
 const apiKey = "92f21b188a400af8bfed32ea5e09220f";
 
 const searchFormCont = document.getElementById("search-form");
 const searchInput = document.querySelector(".search-input");
 const mainWeatherCont = document.querySelector(".main-weather");
+const forecastCont = document.querySelector(".forecast-list");
 
-const getCurrentDate = function () {
-  const today = new Date();
+const getCurrentDate = function (date) {
+  const today = date ? new Date(date) : new Date();
   const options = { weekday: "long", day: "2-digit", month: "short" };
 
   return new Intl.DateTimeFormat("en-US", options).format(today);
@@ -43,11 +44,43 @@ const displayCurrentWeather = function (data) {
   mainWeatherCont.insertAdjacentHTML("afterbegin", html);
 };
 
+const displayForecast = function (data) {
+  const midDayData = data.list.filter((f) => f.dt_txt.includes("12:00:00"));
+  forecastCont.innerHTML = "";
+  midDayData.forEach((day) => {
+    const date = getCurrentDate(day.dt_txt);
+    const [weekday, month, dayNum] = date.split(" ");
+    const [weather] = day.weather;
+    const html = `<li class="forecast-item flex">
+    <div class="flex forecast-temperature">
+      <img
+        src="./assets/images/weather_icons/${weather.icon}.png"
+        alt="${weather.description}"
+        width="46"
+        height="46"
+      />
+      <p>${day.main.temp}°</p>
+    </div>
+    <p>${dayNum} ${month}</p>
+    <p>${weekday.replace(",", "")}</p>
+  </li>`;
+    forecastCont.insertAdjacentHTML("beforeend", html);
+  });
+};
+
 const searchCityWeather = function (city) {
-  fetch(`${apiUrl}?q=${city}&appid=${apiKey}&units=metric`)
+  fetch(`${apiUrl}/weather?q=${city}&appid=${apiKey}&units=metric`)
     .then((response) => response.json())
     .then((data) => {
       displayCurrentWeather(data);
+    });
+};
+
+const searchForecast = function (city) {
+  fetch(`${apiUrl}/forecast?q=${city}&appid=${apiKey}&units=metric`)
+    .then((response) => response.json())
+    .then((data) => {
+      displayForecast(data);
     });
 };
 
@@ -55,4 +88,5 @@ searchFormCont.addEventListener("submit", (e) => {
   e.preventDefault();
   const cityName = searchInput.value;
   searchCityWeather(cityName);
+  searchForecast(cityName);
 });
